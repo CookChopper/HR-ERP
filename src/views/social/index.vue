c848662768693b05be4fb13e7f4935fd82d92c95
remo <15626475734@163.com>
2021-11-30 17:24:18 +0800

    设置临时动态路由

---
+++ src/views/social/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <h2>社保</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Socail'
+}
+</script>
+
+<style scoped>
+
+</style>
